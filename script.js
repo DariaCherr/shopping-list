82e5789a740817328e3d8714aed2a92740d25ab1
DariaCherr <DaskhenCher@mail.ru>
2022-11-05 19:01:53 +0300

    Домашнее задание 4

--- script.js
+++ script.js
@@ -1,3 +1,26 @@
+const sendInput = document.querySelector('#input');
+const listContainer = document.querySelector('#items');
+
+sendInput.addEventListener('keydown', function(event) {
+    if (event.key == 'Enter') {
+        const listText = sendInput.value;
+
+        const newItem = document.createElement('li');
+        newItem.classList.add('items');
+        newItem.textContent = listText;
+
+        newItem.addEventListener('click', function() {
+            newItem.classList.toggle('done');
+        })
+
+        if (listText != '') {
+            listContainer.append(newItem);
+        }
+
+        sendInput.value = '';
+    }
+})
+
 /* Новые элементы должны добавляться в список по нажатию на Enter */
 
 /* Пустые элементы не должны добавляться */
